09f059acd11c47f4edc8c3294c90709d254b6f1a
Aditya Ramesh <adityaramesh15@gmail.com>
2024-06-22 13:02:26 -0500

    Data to DataFrame Functionality added

--- data_manipulation.py
+++ data_manipulation.py
@@ -3,11 +3,12 @@ import random
 from collections import defaultdict
 import shutil
 import pandas as pd
-from PIL import Image
+import numpy as np
+from PIL import Image, ImageStat
 
 '''
 This is a script used to generate percentage-based sample data to preserve relative counts. 
-IMPORTANT: Use a base path with flat directory (each folder is one feature, "featured-data" does this already)
+Use a base path with flat directory (each folder is one feature, "featured-data" does this already).
 Everytime this script is run, 'sample-features' folder will get wiped and recreated with new data. 
 '''
 def random_sample(base_path, sample_proportion):
@@ -38,3 +39,55 @@ def random_sample(base_path, sample_proportion):
             destination_path = os.path.join('sample-data', category)
             shutil.copy(img_path, destination_path)
 
+
+'''
+This is a function used to convert our data into a DataFrame for EDA and Preprocessing purposes. 
+Feature Data being collected includes, width, height, aspect_ratio, mean/std pixel values. 
+This is before preprcoessing, so Numpy NaN data is inputted anywhere the image is corrupted. 
+'''    
+def to_dataframe(base_path): 
+    categories = []
+    names = []
+    widths = []
+    heights = []
+    aspect_ratios = []
+    mean_pixel_values = []
+    std_pixel_values = []
+
+    for category in os.listdir(base_path):
+        category_path = os.path.join(base_path, category)
+        
+        if os.path.isdir(category_path):
+            for img_file in os.listdir(category_path):
+                try:
+                    with Image.open(os.path.join(base_path, category, img_file)) as im:
+                        im.verify()
+                        
+                        width, height = im.size
+                        aspect_ratio = width / height
+
+                        categories.append(category)
+                        widths.append(width)
+                        heights.append(height)
+                        names.append(img_file)
+                        aspect_ratios.append(aspect_ratio)
+
+                        with Image.open(os.path.join(base_path, category, img_file)) as im:
+                            im = im.convert('RGB') 
+                            stat = ImageStat.Stat(im)
+                            mean_pixel_values.append(np.mean(stat.mean))
+                            std_pixel_values.append(np.mean(stat.stddev))
+
+                except (IOError, OSError, Image.UnidentifiedImageError) as e:
+                    categories.append(category)
+                    names.append(img_file)
+                    widths.append(np.nan)
+                    heights.append(np.nan)
+                    aspect_ratios.append(np.nan)
+                    mean_pixel_values.append(np.nan)
+                    std_pixel_values.append(np.nan)
+
+    multi_index = pd.MultiIndex.from_arrays([categories, names], names=('Category', 'Name'))
+    df = pd.DataFrame({'Width': widths, 'Height': heights, 'Aspect Ratio': aspect_ratios, 
+                       "Mean": mean_pixel_values, "Standard Deviation": std_pixel_values}, index=multi_index)
+    return df
